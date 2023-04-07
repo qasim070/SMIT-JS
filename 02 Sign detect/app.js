bb5e66f625e6bb7ede06c9e0d7c7bbde91fc807c
qasim070 <62395180+qasim070@users.noreply.github.com>
2023-04-06 18:36:51 -0700

    Add files via upload

---
+++ 02 Sign detect/app.js
@@ -0,0 +1,13 @@
+let num = +prompt("Enter any Positive or Negative Number");
+
+if (num > 0) {
+    console.log("The number is positive and sign is +" );
+}
+
+else if (num == 0) {
+  console.log("The number is zero");
+}
+
+else {
+     console.log("The number is negative and sign is -");
+}
