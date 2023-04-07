851e951b3429418a5598f2e0bc9f42855f8f39db
qasim070 <62395180+qasim070@users.noreply.github.com>
2023-04-06 18:36:33 -0700

    Add files via upload

---
+++ 01 larger number/app.js
@@ -0,0 +1,9 @@
+let num1 = +prompt("Enter 1st Number");
+let num2 = +prompt("Enter 2nd Number");
+
+
+if(num1 >= num2){
+    console.log( num1 + " is Greater");
+}   else if(num2 >= num1){
+    console.log( num2 + " is Greater");
+}
